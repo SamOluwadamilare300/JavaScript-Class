ac88f50aee4d70dfdefaa6bc88a0b8760b8b5a65
SamOluwadamilare300 <damilarea71@gmail.com>
2024-07-07 12:52:55 -0700

    Javascript Class

---
+++ index.js
@@ -0,0 +1,142 @@
+// console.log(55);
+
+// var strongestMan = "samson"
+// console.log(4500 - 3000);
+
+// console.log(strongestMan);
+
+var a = 17
+var b = 29
+
+
+
+var c = 67
+var d = 56
+
+var e = c**2
+var f = 68 % 5
+
+
+var g = 56
+var h = 24
+
+
+console.log(g-=h);
+
+
+var i = 10
+var j = 4
+
+console.log(i*=j );
+
+var k = 20
+var l = 5
+
+console.log(k/=l);
+
+var m =50
+var n = 2
+
+
+console.log(m**=n);
+
+
+
+
+// console.log(e);
+// console.log(f);
+// console.log(g);
+
+var firstChild = 60
+var secondChild = 82
+var thirdChild = '60'
+
+
+console.log(firstChild > secondChild);
+
+console.log(firstChild < secondChild);
+
+console.log(firstChild <= secondChild);
+
+console.log(firstChild >= secondChild);
+
+console.log(firstChild != secondChild);
+
+console.log(firstChild == secondChild);
+
+console.log(firstChild == thirdChild);
+
+console.log(firstChild === thirdChild);
+
+
+
+// Logical and Boolean Operator 
+
+// &&
+// ||
+
+var tinubu = true 
+var atiku = false 
+var obi = undefined
+var sowore = false 
+
+console.log(tinubu && obi);
+
+console.log(obi || atiku);
+
+// Strings Operators 
+
+var animal = "hippotamus"
+
+console.log(animal.length);
+console.log(animal.toUpperCase());
+
+var firstName = "samson"
+var job = "developer"
+var school = "gmc"
+var dreamJob = "Project Manager"
+
+
+var sentence = " My name is " + firstName + " I am a student of " + school + " and I am a " + job + " I would like to become " + dreamJob
+
+console.log(sentence);
+
+var lastName ="Afolabi"
+var tribe = "Yoruba"
+var clan = "Warriors"
+var place = "Ogbomosho"
+var reside = "Ibadan"
+
+var sentence2 =  lastName +  " is the name of my forebearers " + 
+" from the " + tribe + " tribe " + " we are " + clan + " and Kingmakers originally from "
+ + place + " and we based in " + reside
+
+console.log(sentence2);
+
+
+// Array 
+
+var classMates = ["samson", "babajide", "bawo", "adeyemi"]
+console.log(classMates);
+
+var texting = [55, 72, undefined, null, "lee", "arsenal"]
+console.log(texting.length);
+
+texting[4] = true
+
+
+texting.push("barca")
+texting.pop()
+
+texting.shift()
+texting.unshift("Item")
+
+
+console.log(texting);
+
+
+
+
+
+
+
---
+++ main.js
@@ -0,0 +1,48 @@
+
+
+var firstName = "Afolabi"
+var phoneNumber = 8144954091
+var married = true 
+var numberOfChildren = null
+var numberOfwives = undefined
+var a =5
+var b=18
+var c=b-a
+var d=18
+var e="18"
+var age=23
+var PresidentAge=50
+
+if (age>17) {
+    console.log('eligible to vote');
+
+    
+}else{
+  console.log('ineligible to vote');
+}
+
+
+if (PresidentAge>=50) {
+    console.log('can run for president');
+
+    
+}else{
+  console.log('cant run for president');
+}
+
+
+a=a+b
+console.log(3+2);
+
+// console.log(a<b);
+// console.log(numberOfwives);
+// console.log(firstName);
+// console.log("lastName");
+// console.log("phoneNumber");
+// console.log(c);
+// console.log(5/2);
+// console.log(a);
+// console.log(d >= b);
+// console.log(b==d);
+// console.log(d===e);
+// console.log(d==e);
